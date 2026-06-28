d51b84423eb123dbcfaba04197bff43640b180e1
Marcin4822 <marcin.jaworski4822@gmail.com>
2026-06-29 00:19:42 +0200

    feat: implement task partial update endpoint (#5)

--- TaskFlow.Api/Contracts/Requests/PatchTaskRequest.cs
+++ TaskFlow.Api/Contracts/Requests/PatchTaskRequest.cs
@@ -3,18 +3,16 @@ using TaskFlow.Api.Models;
 
 namespace TaskFlow.Api.Contracts.Requests
 {
-    public class UpdateTaskRequest
+    public class PatchTaskRequest
     {
-        [Required]
         [MinLength(3)]
         [MaxLength(100)]
-        public required string Name { get; set; }
+        public string? Name { get; set; }
 
         [MaxLength(1000)]
         public string? Description { get; set; }
-
         public short? Effort { get; set; }
+        public TaskState? State { get; set; }
 
-        public TaskState State { get; set; }
     }
 }
--- TaskFlow.Api/Models/TaskItem/TaskItem.cs
+++ TaskFlow.Api/Models/TaskItem/TaskItem.cs
@@ -5,18 +5,26 @@ namespace TaskFlow.Api.Models
 {
     public class TaskItem
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public short Effort { get; set; }
-        public TaskState State { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string? Description { get; private set; }
+        public short? Effort { get; private set; }
+        public TaskState State { get; private set; }
 
 
-        public TaskItem(string name, string description)
+        public TaskItem(string name, string? description)
         {
             Name = name;
             Description = description;
             State = TaskState.ToDo;
+        }
+
+        public void Update(string name, string? description, short? effort, TaskState state)
+        {
+            Name = name;
+            Description = description;
+            Effort = effort;
+            State = state;
         }
     }
 }
