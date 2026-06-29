33921d00ea7e1d66c9cd7e2bf77f32809b95ba78
Marcin Jaworski <marcin.jaworski4822@gmail.com>
2026-06-30 00:19:49 +0200

    feat: add global exception handling middleware

---
+++ TaskFlow.Api/Exceptions/TaskNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskFlow.Api.Exceptions
+{
+    public class TaskNotFoundException : Exception
+
+    {
+        public TaskNotFoundException(int taskId) : base($"task with {taskId} was not found")
+        {
+            
+        }
+    }
+}
